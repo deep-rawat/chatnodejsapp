128856e880b56b247297016e6e83b898012f5352
deep-rawat <deepakgamer9521@gmail.com>
2023-05-19 18:21:28 +0530

    code changes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chatnodejsapp.vercel.app",
         methods: ["GET", "POST"],
     }
 });
